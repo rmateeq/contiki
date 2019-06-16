e388777277cf9807db808dd29e00c3b3c8303df8
rmateeq <rmateeq@yahoo.com>
2019-06-16 16:24:57 +0500

    Update broadcast-example.c

--- examples/ipv6/simple-udp-rpl/broadcast-example.c
+++ examples/ipv6/simple-udp-rpl/broadcast-example.c
@@ -1,35 +1,3 @@
-/*
- * Copyright (c) 2011, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
 #include "contiki.h"
 #include "lib/random.h"
 #include "sys/ctimer.h"
@@ -44,8 +12,8 @@
 #include <string.h>
 
 #define UDP_PORT 1234
-
-#define SEND_INTERVAL		(20 * CLOCK_SECOND)
+/*******set 10 instead of 20**********/
+#define SEND_INTERVAL		(10 * CLOCK_SECOND)
 #define SEND_TIME		(random_rand() % (SEND_INTERVAL))
 
 static struct simple_udp_connection broadcast_connection;
@@ -66,9 +34,21 @@ receiver(struct simple_udp_connection *c,
   printf("Data received on port %d from port %d with length %d\n",
          receiver_port, sender_port, datalen);
 }
+/*******print address**********/
+static void
+print_ipv6_addr(const uip_ipaddr_t *ip_addr) {
+    int i;
+    for (i = 0; i < 16; i++) {
+        printf("%02x", ip_addr->u8[i]);
+    }
+}
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(broadcast_example_process, ev, data)
 {
+  /*******PRINTF**********/
+  print("I am server");
+           
   static struct etimer periodic_timer;
   static struct etimer send_timer;
   uip_ipaddr_t addr;
@@ -78,7 +58,9 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
   simple_udp_register(&broadcast_connection, UDP_PORT,
                       NULL, UDP_PORT,
                       receiver);
-
+/*******printf**********/
+  printf("server port: %d. sender port %d.\n",
+         receiver_port, sender_port);
   etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
@@ -87,6 +69,8 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
     printf("Sending broadcast\n");
+   /**********print function***********/
+    print_ipv6_addr(addr);
     uip_create_linklocal_allnodes_mcast(&addr);
     simple_udp_sendto(&broadcast_connection, "Test", 4, &addr);
   }
