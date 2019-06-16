66a35a4048bd2390a6c3b75ba50692f57ce5eb3a
rmateeq <rmateeq@yahoo.com>
2019-06-17 03:56:52 +0500

    Create client.c

---
+++ examples/udp/client.c
@@ -0,0 +1,113 @@
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+
+#include <string.h>
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((u8_t *)addr)[0], ((u8_t *)addr)[1], ((u8_t *)addr)[2], ((u8_t *)addr)[3], ((u8_t *)addr)[4], ((u8_t *)addr)[5], ((u8_t *)addr)[6], ((u8_t *)addr)[7], ((u8_t *)addr)[8], ((u8_t *)addr)[9], ((u8_t *)addr)[10], ((u8_t *)addr)[11], ((u8_t *)addr)[12], ((u8_t *)addr)[13], ((u8_t *)addr)[14], ((u8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF(" %02x:%02x:%02x:%02x:%02x:%02x ",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+
+#define SEND_INTERVAL		3 * CLOCK_SECOND
+#define MAX_PAYLOAD_LEN		40
+
+static struct uip_udp_conn *client_conn;
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client process");
+AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+tcpip_handler(void)
+{
+  char *str;
+
+  if(uip_newdata()) {
+    str = uip_appdata;
+    str[uip_datalen()] = '\0';
+    printf("Response from the server: '%s'\n", str);
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+timeout_handler(void)
+{
+  static int seq_id;
+  char buf[MAX_PAYLOAD_LEN];
+
+  printf("Client sending to: ");
+  PRINT6ADDR(&client_conn->ripaddr);
+  sprintf(buf, "Hello %d from the client", ++seq_id);
+  printf(" (msg: %s)\n", buf);
+  uip_udp_packet_send(client_conn, buf, strlen(buf));
+}
+/*---------------------------------------------------------------------------*/
+static void
+print_local_addresses(void)
+{
+  int i;
+  uip_netif_state state;
+
+  PRINTF("Client IPv6 addresses: ");
+  for(i = 0; i < UIP_CONF_NETIF_MAX_ADDRESSES; i++) {
+    state = uip_netif_physical_if.addresses[i].state;
+    if(state == TENTATIVE || state == PREFERRED) {
+      PRINT6ADDR(&uip_netif_physical_if.addresses[i].ipaddr);
+      PRINTF("\n");
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+set_connection_address(uip_ipaddr_t *ipaddr)
+{
+  // change this IP address depending on the node that runs the server!
+  uip_ip6addr(ipaddr, 0xfe80,0x0000,0x0000, 0x0000, 0xfb00, 0x0012, 0x91b7, 0x9001);
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  static struct etimer et;
+  uip_ipaddr_t ipaddr;
+
+  PROCESS_BEGIN();
+  PRINTF("UDP client process started\n");
+
+  // wait 3 second, in order to have the IP addresses well configured
+  etimer_set(&et, CLOCK_CONF_SECOND*3);
+
+  // wait until the timer has expired
+  PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+
+  print_local_addresses();
+  set_connection_address(&ipaddr);
+
+  /* new connection with remote host */
+  client_conn = udp_new(&ipaddr, HTONS(3000), NULL);
+
+  PRINTF("Created a connection with the server ");
+  PRINT6ADDR(&client_conn->ripaddr);
+  PRINTF("local/remote port %u/%u\n",
+	HTONS(client_conn->lport), HTONS(client_conn->rport));
+
+  etimer_set(&et, SEND_INTERVAL);
+  while(1) {
+    PROCESS_YIELD();
+    if(etimer_expired(&et)) {
+      timeout_handler();
+      etimer_restart(&et);
+    } else if(ev == tcpip_event) {
+      tcpip_handler();
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
