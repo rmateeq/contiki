b34f2d9c56d5174e2b815c31515e3dd1f1c8a5c0
rmateeq <rmateeq@yahoo.com>
2019-06-17 06:48:39 +0500

    Update server.c

--- examples/udp/server.c
+++ examples/udp/server.c
@@ -87,13 +87,14 @@ print_addr2(const uip_ipaddr_t *ip_addr) {
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_server_process, ev, data)
 {
+  uip_ipaddr_t *saddr;
+  uip_ip6addr(saddr, 0xfe80,0x0000,0x0000,0x0000,0xbaae,0xedff,0xfe75,0xbaaf);
+  print_addr2(saddr);
   static struct etimer timer;
   PRINTF("UDP server started\n");
   printf("UDP server started\n");
   PROCESS_BEGIN();
-  uip_ipaddr_t *saddr;
-  uip_ip6addr(saddr, 0xfe80,0x0000,0x0000,0x0000,0xbaae,0xedff,0xfe75,0xbaaf);
-  // ,0x2001,0x06a8,0x1d80, 0x2011, 0xbaae, 0xedff, 0xfe75, 0xb97b
+    // ,0x2001,0x06a8,0x1d80, 0x2011, 0xbaae, 0xedff, 0xfe75, 0xb97b
   //uip_gethostaddr(saddr);
   print_addr2(saddr);
   PRINTF("UDP server started\n");
